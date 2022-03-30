5d2822c54a93139c0e86bb877b2d5bf8d9bca443
Rugile <rugilestasionyte@gmail.com>
2022-03-30 17:38:32 +0300

    first tree

---
+++ js/03-30-nd.js
@@ -0,0 +1,32 @@
+
+/* Nr.1*/
+let n = 20;
+if(n > 0) {
+    console.log('geras');
+} else {console.log('blogas');
+}
+
+/* Nr.2*/
+
+n = 2;
+if(n == 1) {
+    console.log('Eikite');
+} else {console.log('Palaukite')
+}
+
+n = 2;
+if(n =! 2) {
+    console.log('Eikite')
+} else {console.log('Palaukite')
+}
+
+/* Nr. 3 */
+n = 2;
+let d = 3;
+let k = 18;
+n = 5;
+if(d * n >= k) {
+    console.log('Telpa')
+} else {
+    console.log('Netelpa');
+}
